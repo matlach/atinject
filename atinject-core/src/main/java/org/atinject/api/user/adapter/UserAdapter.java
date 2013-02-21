d387978627313710de7cb9dd687cd8944628c58c
matlach <mathieu@mathieulachance.com>
2013-02-20 21:41:55 -0500

    use factory in adapter

--- atinject-core/src/main/java/org/atinject/api/user/adapter/UserAdapter.java
+++ atinject-core/src/main/java/org/atinject/api/user/adapter/UserAdapter.java
@@ -1,7 +1,10 @@
 package org.atinject.api.user.adapter;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
+import org.atinject.api.user.UserDTOFactory;
+import org.atinject.api.user.UserEntityFactory;
 import org.atinject.api.user.dto.User;
 import org.atinject.api.user.entity.UserEntity;
 
@@ -9,12 +12,18 @@ import org.atinject.api.user.entity.UserEntity;
 public class UserAdapter
 {
 
+    @Inject
+    private UserDTOFactory userDTOFactory;
+    
+    @Inject
+    private UserEntityFactory userEntityFactory;
+    
     public User userEntityToUser(UserEntity entity)
     {
         if (entity == null){
             return null;
         }
-        return new User()
+        return userDTOFactory.newUser()
             .setId(entity.getId())
             .setName(entity.getName())
             .setPassword(entity.getPassword());
