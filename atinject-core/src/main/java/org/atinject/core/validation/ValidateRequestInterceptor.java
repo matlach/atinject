4d4525f7c6594f37a6436c0860161e90e66164b9
matlach <mathieu@mathieulachance.com>
2013-03-13 21:39:43 -0400

    distinguish between validate method and validate request
    junit : validate method
    while running : validate request

---
+++ atinject-core/src/main/java/org/atinject/core/validation/ValidateRequestInterceptor.java
@@ -0,0 +1,43 @@
+package org.atinject.core.validation;
+
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
+
+@ValidateMethod
+@Interceptor
+public class ValidateRequestInterceptor
+{
+
+    @Inject
+    private Validator validator;
+
+    @AroundInvoke
+    public Object validateMethod(InvocationContext ctx) throws Exception
+    {
+        Set<ConstraintViolation<Object>> violations = validator.validateParameters(ctx.getTarget(), ctx.getMethod(), ctx.getParameters());
+
+        if (! violations.isEmpty())
+        {
+            throw new ConstraintViolationException(violations);
+        }
+
+        Object result = ctx.proceed();
+
+        violations = validator.validateReturnValue(ctx.getTarget(), ctx.getMethod(), result);
+
+        if (! violations.isEmpty())
+        {
+            throw new ConstraintViolationException(violations);
+        }
+
+        return result;
+    }
+
+}
