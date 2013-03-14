ef68e9bb75e66a1c06b02b7ba2238027d5278db7
matlach <mathieu@mathieulachance.com>
2013-03-13 21:51:44 -0400

    added missing annotations

--- atinject-core/src/main/java/org/atinject/core/tiers/WebSocketService.java
+++ atinject-core/src/main/java/org/atinject/core/tiers/WebSocketService.java
@@ -4,11 +4,13 @@ import org.atinject.core.exception.HandleWebSocketServiceException;
 import org.atinject.core.profiling.Profile;
 import org.atinject.core.thread.ThreadTracker;
 import org.atinject.core.transaction.Transactional;
+import org.atinject.core.validation.ValidateRequest;
 
 @HandleWebSocketServiceException
 @Profile
 @ThreadTracker
 @Transactional
+@ValidateRequest
 public abstract class WebSocketService {
 
 
--- atinject-core/src/main/java/org/atinject/core/validation/ValidateMethod.java
+++ atinject-core/src/main/java/org/atinject/core/validation/ValidateMethod.java
@@ -1,5 +1,6 @@
 package org.atinject.core.validation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
@@ -10,6 +11,7 @@ import javax.interceptor.InterceptorBinding;
 
 @InterceptorBinding
 @Inherited
+@Documented
 @Target( { ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidateMethod
