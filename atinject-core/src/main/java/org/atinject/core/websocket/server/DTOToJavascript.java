57e39afa15337eb78aaef9c12dd2c54ad427d045
matlach <mathieu@mathieulachance.com>
2013-04-09 20:35:02 -0400

    added validation, recreated create alias

--- atinject-core/src/main/java/org/atinject/core/websocket/server/DTOToJavascript.java
+++ atinject-core/src/main/java/org/atinject/core/websocket/server/DTOToJavascript.java
@@ -1,9 +1,12 @@
 package org.atinject.core.websocket.server;
 
+import java.io.PrintWriter;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
@@ -33,6 +36,9 @@ public class DTOToJavascript
     
     @PostConstruct
     public void initialize(){
+        
+        validateDtoClassSimpleNameDuplicate();
+        
         List<Class<? extends DTO>> dtoClasses = dtoRegistryExtension.getClasses();
         javascript = "";
         for (Class<? extends DTO> dtoClass : dtoClasses){
@@ -48,57 +54,98 @@ public class DTOToJavascript
         // that way we could ease frontend development ?
     }
     
+    public void validateDtoClassSimpleNameDuplicate(){
+        Set<String> dtoClassesSimpleName = new LinkedHashSet<>();
+        List<Class<? extends DTO>> dtoClasses = dtoRegistryExtension.getClasses();
+        for (Class<? extends DTO> dtoClass : dtoClasses){
+            if (! dtoClassesSimpleName.add(dtoClass.getSimpleName())){
+                // TODO add which classes are responsible
+                // TODO throw only after full analysis
+                throw new RuntimeException("duplicate found");
+            }
+        }
+    }
+    
     public String getGeneratedJavascript(){
         return this.javascript;
     }
     
+    private static void saveJavascript(String path, String javascript){
+        try {
+            PrintWriter writer = new PrintWriter(path);
+            writer.write(javascript);
+            writer.close();
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    
     public String generate(Class<? extends DTO> dtoClass){
         if (dtoClass.equals(DTO.class)){
-            return generateDTO(DTO.class);
+            String javascript = generateDTO(DTO.class);
+            String path = "/Users/mathieu/Desktop/generated-code/" + DTO.class.getSimpleName() + ".js";
+            saveJavascript(path, javascript);
+            return javascript;
         }
         else if (dtoClass.equals(WebSocketRequest.class)){
-            return generateBaseWebSocketRequest(WebSocketRequest.class);
+            String javascript = generateBaseWebSocketRequest(WebSocketRequest.class);
+            String path = "/Users/mathieu/Desktop/generated-code/" + WebSocketRequest.class.getSimpleName() + ".js";
+            saveJavascript(path, javascript);
+            return javascript;
         }
         else if (dtoClass.equals(WebSocketNotification.class)){
-            return generateBaseWebSocketNotification(WebSocketNotification.class);
+            String javascript = generateBaseWebSocketNotification(WebSocketNotification.class);
+            String path = "/Users/mathieu/Desktop/generated-code/" + WebSocketNotification.class.getSimpleName() + ".js";
+            saveJavascript(path, javascript);
+            return javascript;
         }
         else if (dtoClass.equals(WebSocketResponse.class)){
-            return generateBaseWebSocketResponse(WebSocketResponse.class);
+            String javascript = generateBaseWebSocketResponse(WebSocketResponse.class);
+            String path = "/Users/mathieu/Desktop/generated-code/" + WebSocketResponse.class.getSimpleName() + ".js";
+            saveJavascript(path, javascript);
+            return javascript;
         }
         else if (WebSocketRequest.class.isAssignableFrom(dtoClass)){
-            return generateRequest((Class<? extends WebSocketRequest>) dtoClass);
+            String javascript = generateRequest((Class<? extends WebSocketRequest>) dtoClass);
+            String path = "/Users/mathieu/Desktop/generated-code/" + dtoClass.getSimpleName() + ".js";
+            saveJavascript(path, javascript);
+            return javascript;
         }
         else if (WebSocketNotification.class.isAssignableFrom(dtoClass)){
-            return generateNotification((Class<? extends WebSocketNotification>) dtoClass);
+            String javascript = generateNotification((Class<? extends WebSocketNotification>) dtoClass);
+            String path = "/Users/mathieu/Desktop/generated-code/" + dtoClass.getSimpleName() + ".js";
+            saveJavascript(path, javascript);
+            return javascript;
         }
         else if (WebSocketResponse.class.isAssignableFrom(dtoClass)){
-            return generateResponse((Class<? extends WebSocketResponse>) dtoClass);
+            String javascript = generateResponse((Class<? extends WebSocketResponse>) dtoClass);
+            String path = "/Users/mathieu/Desktop/generated-code/" + dtoClass.getSimpleName() + ".js";
+            saveJavascript(path, javascript);
+            return javascript;
         }
         else if (DTO.class.isAssignableFrom(dtoClass)){
-            return generateComplexDTO(dtoClass);
+            String javascript = generateComplexDTO(dtoClass);
+            String path = "/Users/mathieu/Desktop/generated-code/" + dtoClass.getSimpleName() + ".js";
+            saveJavascript(path, javascript);
+            return javascript;
         }
         throw new RuntimeException();
     }
     
     public static final String dtoTemplate =
-"function ${dtoClassSimpleName}() {" + NEWLINE +
-"};" + NEWLINE +
-"" + NEWLINE +
-"${dtoClassSimpleName}.prototype.getClass = function() {" + NEWLINE +
-"    return this.${jsonTypeInfoProperty};" + NEWLINE +
-"}" + NEWLINE;
+"define([]," + NEWLINE +
+"function(){" + NEWLINE +
+"    function ${dtoClassSimpleName}() {" + NEWLINE +
+"    };" + NEWLINE +
+"    " + NEWLINE +
+"    ${dtoClassSimpleName}.prototype.getClass = function() {" + NEWLINE +
+"        return this.${jsonTypeInfoProperty};" + NEWLINE +
+"    };" + NEWLINE +
+"    " + NEWLINE +
+"    return ${dtoClassSimpleName};" + NEWLINE +
+"});" + NEWLINE;
     
-    /**
-     * expected result :
-     * <pre>
-     * function DTO() {
-     * };
-     *
-     * DTO.prototype.getClass = function() {
-     *     return this._class;
-     * };
-     * </pre>
-     */
     public static String generateDTO(Class<DTO> dtoClass){
         JsonTypeInfo jsonTypeInfoAnnotation = dtoClass.getAnnotation(JsonTypeInfo.class);
         String dtoClassSimpleName = dtoClass.getSimpleName();
@@ -153,31 +200,54 @@ public class DTOToJavascript
     }
 
     public static final String complexDtoTemplate =
-"function ${dtoClassSimpleName}() {" + NEWLINE +
-"    this._class = \"${dtoClassCanonicalName}\"" + NEWLINE +
-"};" + NEWLINE +
+"define([${dtoDependenciesName}]," + NEWLINE +
+"function(${dtoDependenciesValue}){" + NEWLINE +
+"    function ${dtoClassSimpleName}() {" + NEWLINE +
+"        this._class = \"${dtoClassCanonicalName}\"" + NEWLINE +
+"    };" + NEWLINE +
 "" + NEWLINE +
-"${dtoClassSimpleName}.prototype = new ${dtoSuperClassSimpleName}();" + NEWLINE +
+"    ${dtoClassSimpleName}.prototype = new ${dtoSuperClassSimpleName}();" + NEWLINE +
 "" + NEWLINE +
-"${dtoGetterSetters}" +  NEWLINE;
+"    ${dtoGetterSetters}" +  NEWLINE +
+"    " + NEWLINE +
+"    return ${dtoClassSimpleName};" + NEWLINE +
+"});" + NEWLINE;
     
     public static final String getterTemplate =
-"${dtoClassSimpleName}.prototype.get${capitalizedProperty} = function() {" + NEWLINE +
-"    return this.${property};" + NEWLINE +
-"};" + NEWLINE +
+"    ${dtoClassSimpleName}.prototype.get${capitalizedProperty} = function() {" + NEWLINE +
+"        return this.${property};" + NEWLINE +
+"    };" + NEWLINE +
 "" +  NEWLINE;
     
     public static final String setterTemplate =
-"${dtoClassSimpleName}.prototype.set${capitalizedProperty} = function(${property}) {" + NEWLINE +
-"    this.${property} = ${property};" + NEWLINE +
-"    return this;" + NEWLINE +
-"};" + NEWLINE +
+"    ${dtoClassSimpleName}.prototype.set${capitalizedProperty} = function(${property}) {" + NEWLINE +
+"        this.${property} = ${property};" + NEWLINE +
+"        return this;" + NEWLINE +
+"    };" + NEWLINE +
 "" +  NEWLINE;
     
     private static String generateComplexDTO(Class<? extends DTO> dtoClass){
         String dtoClassSimpleName = dtoClass.getSimpleName();
         String dtoClassCanonicalName = dtoClass.getCanonicalName();
         String dtoSuperClassSimpleName = dtoClass.getSuperclass().getSimpleName();
+        
+        Set<Class<?>> dependencies = getDependencies(dtoClass);
+        
+        String dtoDependenciesName = "";
+        for (Class<?> dependency : dependencies){
+            dtoDependenciesName = dtoDependenciesName + "\"" + dependency.getSimpleName() + "\", ";
+        }
+        if (! dtoDependenciesName.isEmpty()){
+            dtoDependenciesName = dtoDependenciesName.substring(0, dtoDependenciesName.lastIndexOf(","));
+        }
+        
+        String dtoDependenciesValue = "";
+        for (Class<?> dependency : dependencies){
+            dtoDependenciesValue = dtoDependenciesValue + dependency.getSimpleName() + ", ";
+        }
+        if (! dtoDependenciesName.isEmpty()){
+            dtoDependenciesValue = dtoDependenciesValue.substring(0, dtoDependenciesValue.lastIndexOf(","));
+        }
         
         String getterSetters = "";
         for (Field field : getDeclaredNonTransientNonStaticFields(dtoClass)){
@@ -194,6 +264,8 @@ public class DTOToJavascript
             getterSetters = getterSetters + getter + setter;
         }
         String generated = complexDtoTemplate
+                .replace("${dtoDependenciesName}", dtoDependenciesName)
+                .replace("${dtoDependenciesValue}", dtoDependenciesValue)
                 .replace("${dtoClassSimpleName}", dtoClassSimpleName)
                 .replace("${dtoClassCanonicalName}", dtoClassCanonicalName)
                 .replace("${dtoSuperClassSimpleName}", dtoSuperClassSimpleName)
@@ -202,10 +274,48 @@ public class DTOToJavascript
         return generated;
     }
     
+public static final String CLASS_ALIAS_TEMPLATE =
+"define([${dtoDependenciesName}]," + NEWLINE +
+"function(${dtoDependenciesValue}){" + NEWLINE +
+"    var classAlias = new Array();" + NEWLINE +
+"    ${dtoAliases}" + NEWLINE +
+"    return classAlias;" + NEWLINE +
+"});" + NEWLINE;
+
+    public String generateRequireJs(){
+        List<Class<? extends DTO>> dtoClasses = dtoRegistryExtension.getClasses();
+        
+        String dtoDependenciesName = "";
+        for (Class<? extends DTO> dtoClass : dtoClasses){
+            dtoDependenciesName = dtoDependenciesName + "\"" + dtoClass.getSimpleName() + "\", ";
+        }
+        dtoDependenciesName = dtoDependenciesName.substring(0, dtoDependenciesName.lastIndexOf(","));
+
+        String dtoDependenciesValue = "";
+        for (Class<? extends DTO> dtoClass : dtoClasses){
+            dtoDependenciesName = dtoDependenciesName + "\"" + dtoClass.getSimpleName() + "\", ";
+        }
+        dtoDependenciesName = dtoDependenciesName.substring(0, dtoDependenciesName.lastIndexOf(","));        
+        
+        return CLASS_ALIAS_TEMPLATE
+                .replace("${dtoDependenciesName}", dtoDependenciesName)
+                .replace("${dtoDependenciesValue}", dtoDependenciesValue)
+                .replace("${dtoAliases}", "");
+    }
+    
+    public static final String CLASS_ALIAS_ARRAY_TEMPLATE =
+"    ${dtoClassSimpleName}: \"src/org/atinject/dto/${dtoClassSimpleName}\"," + NEWLINE;
+    
+    public static String generateRequireJsPaths(Class<? extends DTO> dtoClass){
+        String dtoClassSimpleName = dtoClass.getSimpleName();
+        return CLASS_ALIAS_ARRAY_TEMPLATE
+                .replace("${dtoClassSimpleName}", dtoClassSimpleName);
+    }
+    
     public static final String aliasTemplate =
 "classAlias[\"${dtoClassCanonicalName}\"] = ${dtoClassSimpleName};" + NEWLINE;
     
-    public static String generateAlias(Class<? extends DTO> dtoClass){
+    public String generateAlias(Class<? extends DTO> dtoClass){
         String dtoClassCanonicalName = dtoClass.getCanonicalName();
         String dtoClassSimpleName = dtoClass.getSimpleName();
         return aliasTemplate
@@ -223,6 +333,17 @@ public class DTOToJavascript
             fields.add(field);
         }
         return fields;
+    }
+    
+    private static Set<Class<?>> getDependencies(Class<? extends DTO> dtoClass){
+        Set<Class<?>> dependencies = new LinkedHashSet<>();
+        dependencies.add(dtoClass.getSuperclass());
+        for (Field field : getDeclaredNonTransientNonStaticFields(dtoClass)){
+            if (DTO.class.isAssignableFrom(field.getType())){
+                dependencies.add(field.getType());
+            }
+        }
+        return dependencies;
     }
     
     private static String capitalizeFirstLetter(String string){
