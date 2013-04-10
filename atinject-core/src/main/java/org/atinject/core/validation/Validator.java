2ddd0b93a0c9ad33b2563ee2f97f6b1a737c3ba4
matlach <mathieu@mathieulachance.com>
2013-04-09 22:12:31 -0400

    fixed validator

--- atinject-core/src/main/java/org/atinject/core/validation/Validator.java
+++ atinject-core/src/main/java/org/atinject/core/validation/Validator.java
@@ -3,18 +3,26 @@ package org.atinject.core.validation;
 import java.lang.reflect.Method;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
 import javax.validation.executable.ExecutableValidator;
 
 @ApplicationScoped
 public class Validator
 {
-    
-    @Inject private javax.validation.Validator validator;
-    @Inject private ExecutableValidator executableValidator;
 
+    // XXX find why injection is not working anymore ?
+    private javax.validation.Validator validator;
+    private ExecutableValidator executableValidator;
+    
+    @PostConstruct
+    public void initialize(){
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+        executableValidator = validator.forExecutables();
+    }
+    
     public <T> Set<ConstraintViolation<T>> validateParameters(T object, Method method, Object[] parameterValues) {
         return executableValidator.validateParameters(object, method, parameterValues);
     }
