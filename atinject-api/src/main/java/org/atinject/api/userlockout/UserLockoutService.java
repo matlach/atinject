1d4b5b335b3929fb151dcc68182733ccc715bdb4
matlach <mathieu@mathieulachance.com>
2013-09-19 21:07:22 -0400

    added features to implements

--- atinject-api/src/main/java/org/atinject/api/userlockout/UserLockoutService.java
+++ atinject-api/src/main/java/org/atinject/api/userlockout/UserLockoutService.java
@@ -1,11 +1,29 @@
 package org.atinject.api.userlockout;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 
+import org.atinject.api.authentication.event.AuthenticationFailed;
 import org.atinject.core.tiers.Service;
 
 @ApplicationScoped
 public class UserLockoutService extends Service {
 
     // Threshold 5times, duration 30mins, reset duration 5 minutes.
+	
+	public boolean isUserLocked(String userId){
+		return false;
+	}
+	
+	public void lockUser(String userId){
+		
+	}
+	
+	public void unlockUser(String userId){
+		
+	}
+	
+	public void onAuthenticationFailed(@Observes AuthenticationFailed event){
+		// increment authentication failed count
+	}
 }
