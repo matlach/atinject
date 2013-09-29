dd388d95f8a0bac3f448b97433476148af8b3307
matlach <mathieu@mathieulachance.com>
2013-09-29 13:32:45 -0400

    @Startup is not a qualifier

--- atinject-core/src/main/java/org/atinject/core/startup/Startup.java
+++ atinject-core/src/main/java/org/atinject/core/startup/Startup.java
@@ -6,9 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import javax.inject.Qualifier;
-
-@Qualifier
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
