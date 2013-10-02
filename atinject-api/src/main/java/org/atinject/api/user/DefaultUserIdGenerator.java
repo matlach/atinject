e6501f86a73c028170bd892322896d6b740aa3fd
matlach <mathieu@mathieulachance.com>
2013-10-01 22:14:07 -0400

    added todo

--- atinject-api/src/main/java/org/atinject/api/user/DefaultUserIdGenerator.java
+++ atinject-api/src/main/java/org/atinject/api/user/DefaultUserIdGenerator.java
@@ -9,6 +9,8 @@ public class DefaultUserIdGenerator implements UserIdGenerator {
 
 	@Override
 	public String generateUserId() {
+		// TODO replace random uuid with version 1 uuid
+		// cassandra uses(?) http://johannburkard.de/software/uuid/#maven
 		return UUID.randomUUID().toString();
 	}
 
