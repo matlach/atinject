0b4b2bf5c201cb05ac823ff2ffe79b022eae5c41
matlach <mathieu@mathieulachance.com>
2013-10-27 17:10:08 -0400

    translation service

---
+++ atinject-core/src/main/java/org/atinject/core/translation/entity/Translation.java
@@ -0,0 +1,13 @@
+package org.atinject.core.translation.entity;
+
+public class Translation {
+
+    String id;
+    
+    String locale;
+    
+    String text;
+    
+    String context;
+    
+}
---
+++ atinject-core/src/main/java/org/atinject/core/translation/enumeration/Locale.java
@@ -0,0 +1,5 @@
+package org.atinject.core.translation.enumeration;
+
+public class Locale {
+
+}
