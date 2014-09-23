6629d124fb3a59606113e33794e3b230662b5a17
matlach <mathieu@mathieulachance.com>
2014-09-23 19:48:06 -0400

    introduce managed executor

---
+++ atinject-core/src/main/java/org/atinject/core/concurrent/AsynchronousService.java
@@ -0,0 +1,47 @@
+package org.atinject.core.concurrent;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class AsynchronousService extends ManagedExecutorService {
+
+    /**
+     * @see Executors#newFixedThreadPool(int)
+     * @see ThreadPoolExecutor#allowCoreThreadTimeOut(boolean)
+     */
+    @PostConstruct
+    public void initialize() {
+        threadPoolExecutor = new ThreadPoolExecutor(100, 100,
+                60L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(),
+                new ContextAwareThreadFactory());
+        // TODO implements RejectedExecutionHandler
+        threadPoolExecutor.allowCoreThreadTimeOut(true);
+    }
+    
+    @PreDestroy
+    public void cleanUp() {
+        shutdown();
+        try {
+            awaitTermination(60L, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
