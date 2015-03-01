c371e06bbf63cc0ca9643e4f6c0b38fe59975e6a
matlach <mathieu@mathieulachance.com>
2015-02-28 19:39:55 -0500

    use @WithAnnotations

--- atinject-core/src/main/java/org/atinject/core/startup/StartupExtension.java
+++ atinject-core/src/main/java/org/atinject/core/startup/StartupExtension.java
@@ -3,31 +3,24 @@ package org.atinject.core.startup;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.AfterDeploymentValidation;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessBean;
+import javax.enterprise.inject.spi.WithAnnotations;
 
-public class StartupExtension implements Extension
-{
+public class StartupExtension implements Extension {
+	
     private final Set<Bean<?>> startupBeans = new LinkedHashSet<>();
 
-    <X> void processBean(@Observes ProcessBean<X> event)
-    {
-       if (event.getAnnotated().isAnnotationPresent(Startup.class) &&
-             event.getAnnotated().isAnnotationPresent(ApplicationScoped.class))
-       {
-          startupBeans.add(event.getBean());
-       }
+    <X> void processBean(@Observes @WithAnnotations(Startup.class) ProcessBean<X> event) {
+        startupBeans.add(event.getBean());
     }
     
-    void afterDeploymentValidation(@Observes AfterDeploymentValidation event, BeanManager manager)
-    {
-       for (Bean<?> bean : startupBeans)
-       {
+    void afterDeploymentValidation(@Observes AfterDeploymentValidation event, BeanManager manager) {
+       for (Bean<?> bean : startupBeans) {
           // the call to toString() is a cheat to force the bean to be initialized
           manager.getReference(bean, bean.getBeanClass(), manager.createCreationalContext(bean)).toString();
        }
