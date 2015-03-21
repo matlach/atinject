78069ac85ce93999018fcbb5a56451b8c2a25cf1
matlach <mathieu@mathieulachance.com>
2015-03-21 12:27:37 -0400

    cleanup

--- atinject-core/src/main/java/org/atinject/core/cdi/DefaultJavaSECDIProvider.java
+++ atinject-core/src/main/java/org/atinject/core/cdi/DefaultJavaSECDIProvider.java
@@ -5,20 +5,13 @@ import org.jboss.weld.environment.se.Weld;
 public final class DefaultJavaSECDIProvider {
 
     private DefaultJavaSECDIProvider() {
-        
+        // utility class
     }
     
     private static Weld weld;
     
     public static void initialize() {
-        weld = new Weld(){
-//            @Override
-//            protected Deployment createDeployment(ResourceLoader resourceLoader, CDI11Bootstrap bootstrap) {
-//                Deployment deployment = super.createDeployment(resourceLoader, bootstrap);
-//                deployment.getServices().add(TransactionServices.class, new InMemoryTransactionServices());
-//                return deployment;
-//            }
-        };
+        weld = new Weld();
         weld.initialize();
         
         Runtime.getRuntime()
