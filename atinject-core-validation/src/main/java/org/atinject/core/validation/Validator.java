b0a58dcd2da98676c3aa037dda5afc5ada688c62
matlach <mathieu@mathieulachance.com>
2015-04-26 08:50:47 -0400

    make validation cdi aware

--- atinject-core-validation/src/main/java/org/atinject/core/validation/Validator.java
+++ atinject-core-validation/src/main/java/org/atinject/core/validation/Validator.java
@@ -5,27 +5,41 @@ import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorFactory;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.executable.ExecutableValidator;
 
+import org.atinject.core.cdi.CDI;
 import org.hibernate.validator.HibernateValidator;
 import org.hibernate.validator.parameternameprovider.ReflectionParameterNameProvider;
 
 @ApplicationScoped
 public class Validator {
 
-    // XXX find why injection is not working anymore ?
     private javax.validation.Validator validator;
     private ExecutableValidator executableValidator;
     
     @PostConstruct
-    public void initialize(){
+    public void initialize() {
         validator = Validation.byProvider(HibernateValidator.class)
         		.configure()
         		.parameterNameProvider(new ReflectionParameterNameProvider())
         		//TODO .messageInterpolator(interpolator)
         		.failFast(true)
+        		.constraintValidatorFactory(new ConstraintValidatorFactory() {
+					
+					@Override
+					public void releaseInstance(ConstraintValidator<?, ?> instance) {
+						// no-op
+					}
+					
+					@Override
+					public <T extends ConstraintValidator<?, ?>> T getInstance(Class<T> key) {
+						return CDI.select(key).get();
+					}
+				})
         		.buildValidatorFactory()
         		.getValidator();
         executableValidator = validator.forExecutables();
